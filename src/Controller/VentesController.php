3d2345100a5c8560464c484a3585f59c82203dda
Nandoby <nandobiaccalli@gmail.com>
2021-10-27 00:44:25 +0200

    ajout du controller ventes + entité voiture

---
+++ src/Controller/VentesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VentesController extends AbstractController
+{
+    /**
+     * @Route("/ventes", name="ventes")
+     */
+    public function index(): Response
+    {
+        return $this->render('ventes/index.html.twig', [
+            'controller_name' => 'VentesController',
+        ]);
+    }
+}
---
+++ src/Entity/Voiture.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VoitureRepository::class)
+ */
+class Voiture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $modele;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $km;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nombres_proprietaires;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $cylindree;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $puissance;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $mise_circulation;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $carburant;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $transmission;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $options;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getKm(): ?int
+    {
+        return $this->km;
+    }
+
+    public function setKm(int $km): self
+    {
+        $this->km = $km;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getNombresProprietaires(): ?int
+    {
+        return $this->nombres_proprietaires;
+    }
+
+    public function setNombresProprietaires(?int $nombres_proprietaires): self
+    {
+        $this->nombres_proprietaires = $nombres_proprietaires;
+
+        return $this;
+    }
+
+    public function getCylindree(): ?string
+    {
+        return $this->cylindree;
+    }
+
+    public function setCylindree(string $cylindree): self
+    {
+        $this->cylindree = $cylindree;
+
+        return $this;
+    }
+
+    public function getPuissance(): ?string
+    {
+        return $this->puissance;
+    }
+
+    public function setPuissance(string $puissance): self
+    {
+        $this->puissance = $puissance;
+
+        return $this;
+    }
+
+    public function getMiseCirculation(): ?\DateTimeInterface
+    {
+        return $this->mise_circulation;
+    }
+
+    public function setMiseCirculation(?\DateTimeInterface $mise_circulation): self
+    {
+        $this->mise_circulation = $mise_circulation;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): self
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+
+    public function getTransmission(): ?string
+    {
+        return $this->transmission;
+    }
+
+    public function setTransmission(string $transmission): self
+    {
+        $this->transmission = $transmission;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getOptions(): ?string
+    {
+        return $this->options;
+    }
+
+    public function setOptions(string $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
